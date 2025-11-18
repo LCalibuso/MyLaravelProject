87acea1e14df610f78ff730db9cb1eb9aa3987df
MegaDran <sclarecalibusoleonard@gmail.com>
2025-11-18 10:59:28 +0800

    hopefully i retrieved the lost codes in routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+Route::get('/', function () {
+    return view('welcome');
+    return view('home');
+});
+
+// Route::get('/home', function () {
+//     return view('home');
+// });
+
 Route::get('/', function(){
     return view('register');
 });
